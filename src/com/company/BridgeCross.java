cfa040eacfbe7788a408fb7284d1f6519893d360
Stephen Meidanis <40611651+StephenDanys@users.noreply.github.com>
2020-12-12 11:59:44 +0200

    Update BridgeCross.java

--- src/com/company/BridgeCross.java
+++ src/com/company/BridgeCross.java
@@ -5,31 +5,48 @@ import java.util.*;
 public class BridgeCross {
 
     public static void main(String[] args) {
-
-        ArrayList<Integer> A=new ArrayList<>();
-        ArrayList<Integer> B=new ArrayList<>();
+        //checking values of parametres
+        for( String s:args) { 
+            if (Integer.parseInt(s)<= 0){
+                System.println.out("Negative parameter given");
+                System.exit(0);
+            } 
+        }        
         // 1st arg: people number. then idividual people crossing times. Lastly Time limit.
-        int n= Integer.parseInt(args[0]);
-        int Time= Integer.parseInt(args[n+1]);
+        
+        int n= Integer.parseInt(args[0]); //number of family members
+        if (n!= args.lengh()+1) {
+            System.println.out("Wrong number of parameters given");
+            System.exit(0);
+        }
+        int Time= Integer.parseInt(args.legth()-1); //time limit
+        //initializing lists
+        ArrayList<Integer> A=new ArrayList<>();//list to save time of family members
+        ArrayList<Integer> B=new ArrayList<>();//empty list to pass as value to initial State
         for (int i=1;i<=n; i++) A.add(Integer.parseInt(args[i]));
         Collections.sort(A);
+
+        //initiallizing class objects
         State initial=new State(A,B,0,true);
         Solver solver=new Solver();
+
+        //solution starts
         State terminal=solver.Astar(initial);
-        if(terminal==null) System.out.println("apotyxia");
+
+        //results
+        if(terminal==null) System.out.println("No solution found");
         else
         {
             if (terminal.getElapsedTime()> Time) {
-               System.out.println("no solution found in given time by A*"); 
+               System.out.println("no solution found in given time"); 
             } 
             else 
             {
-                System.out.println("Terminal State reached Using A*. Elapsed Time= " + terminal.getElapsedTime());
+                System.out.println("Terminal State reached. Elapsed Time= " + terminal.getElapsedTime());
                 System.out.println("Route to initial:");
                 solver.findPath(terminal).forEach(State::print);
-
             }
         }
-
-        }
     }
+}
+    
