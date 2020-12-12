0015e80c7e93cabdf0878043e14738616c795b53
Stephen Meidanis <40611651+StephenDanys@users.noreply.github.com>
2020-12-12 12:09:17 +0200

    Update Solver.java

--- src/com/company/Solver.java
+++ src/com/company/Solver.java
@@ -9,13 +9,16 @@ public class Solver {
     private ArrayList<State> states;
     private HashSet<State> closedSet;
     private MinPQ<State> queue;
+    
+    //constuctor
     public Solver()
     {
         this.states = new ArrayList<State>();
         this.closedSet = new HashSet<State>();
     }
 
-    public List<State>  findPath(State solution)
+    //method for finding the path from solution to initial state
+    public List<State> findPath(State solution)
     {
         LinkedList<State> ll=new LinkedList<State>();
         while (solution!=null)
@@ -26,27 +29,26 @@ public class Solver {
         return ll;
     }
 
+    //implemetation of Astar using a minPriorityQueue
     public State Astar(State initialState)
     {
-        this.queue =new MinPQ<State>(); //xreiazomaste ton constuctor me to comparator. To priority twn states einai h synasthsh toy comparator
-        if(initialState.isTerminal()) return initialState;
+        this.queue =new MinPQ<State>(); //pq object
+        if(initialState.isTerminal()) return initialState; //checking initial state 
 
         queue.insert(initialState);
         while(!queue.isEmpty())
         {
-            State current= queue.delMin();
+            State current= queue.delMin(); //examine State with least prioroty
             if(current.isTerminal()) return current;
             for(State s:current.getChildren())
             {
-            if(!closedSet.contains(s)) queue.insert(s);
+                if(!closedSet.contains(s)) {
+                    queue.insert(s); //if children aren't in closed set, insert them in queue
+                    closedSet.add(s);// add them to closedset.
+                }
             }
         }
-
         return null;
-
     }
-
-
 }
-
 
